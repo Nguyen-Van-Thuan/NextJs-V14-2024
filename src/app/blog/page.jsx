3bbb1c3700e6c3f4cef4591cc16e5c6644ad05eb
thuan.nv <nguyenthuankma@gmail.com>
2024-06-10 16:18:46 +0700

    add router api blogs

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -2,10 +2,24 @@ import PostCard from "@/component/postCard/PostCard";
 import styles from "./blog.module.css";
 import { getPosts } from "@/lib/data";
 
+// FETCH DATA WITH AN API
+const getData = async () => {
+  const res = await fetch("http://localhost:3000/api/blog", {next:{revalidate:3600}});
+
+  if (!res.ok) {
+    throw new Error("Something went wrong");
+  }
+
+  return res.json();
+};
+
 const BlogPage = async () => {
 
+  // FETCH DATA WITH AN API
+  const posts = await getData();
+
   // FETCH DATA WITHOUT AN API
-  const posts = await getPosts();
+  // const posts = await getPosts();
 
   return (
     <div className={styles.container}>
