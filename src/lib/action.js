d6a7af8650bd6b73ae88de7dc30ec0867f02aae2
thuan.nv <nguyenthuankma@gmail.com>
2024-06-12 14:38:56 +0700

    Update action.js

--- src/lib/action.js
+++ src/lib/action.js
@@ -54,3 +54,43 @@ export const handleLogout = async () => {
   "use server";
   await signOut();
 };
+
+
+// Hàm được gọi khi người dùng muốn đăng ký
+
+export const register = async (previousState, formData) => {
+  const { username, email, password, img, passwordRepeat } =
+    Object.fromEntries(formData);
+
+  if (password !== passwordRepeat) {
+    return { error: "Passwords do not match" };
+  }
+
+  try {
+    connectToDb();
+
+    const user = await User.findOne({ username });
+
+    if (user) {
+      return { error: "Username already exists" };
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+      img,
+    });
+
+    await newUser.save();
+    console.log("saved to db");
+
+    return { success: true };
+  } catch (err) {
+    console.log(err);
+    return { error: "Something went wrong!" };
+  }
+};
