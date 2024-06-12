76fc80cabd3ec154872427be7c47d36561d39a6d
ThuanVan Nguyen <nguyenthuankma@gmail.com>
2024-06-13 01:02:11 +0700

    Complete the project

--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -1,14 +1,17 @@
 import { auth } from "@/lib/auth";
 import Links from "./links/Links";
 import styles from "./navbar.module.css";
+import Link from "next/link";
 
 const Navbar = async () => {
   // session thông tin user đăng nhập bằng github
   const session = await auth();
-  
+
   return (
     <div className={styles.container}>
-      <div className={styles.logo}>Logo</div>
+      <div className={styles.logo}>
+        <Link href="/">Logo</Link>
+      </div>
       <div>
         <Links session={session} />
       </div>
