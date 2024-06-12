9628fb71059402ce643df54645900e12d7de75b9
ThuanVan Nguyen <nguyenthuankma@gmail.com>
2024-06-13 01:05:34 +0700

    Update Footer.jsx

--- src/component/footer/Footer.jsx
+++ src/component/footer/Footer.jsx
@@ -3,9 +3,9 @@ import styles from "./footer.module.css";
 const Footer = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.logo}>THUAN.NV</div>
+      <div className={styles.logo}>FULLSTACK</div>
       <div className={styles.text}>
-        NEXT.JS 14 FULLSTACK © All rights reserved.
+        NEXT.JS 14 BY THUAN.VN © All rights reserved.
       </div>
     </div>
   );
